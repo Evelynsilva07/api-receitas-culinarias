8a97ead445ef0ca13e610d63253f978e2a025547
Evelynsilva07 <evelyndsilva07@gmail.com>
2025-12-14 20:17:07 -0300

    feat: adiciona lista de receitas

--- app.py
+++ app.py
@@ -6,5 +6,16 @@ app = Flask(__name__)
 def home():
     return jsonify({"mesagem": "API de Receitas de Culinária"})
 
+receitas = [
+    {
+        "id": 1,
+        "nome": "Bolo de Chocolate",
+        "ingredientes": ["farinha", "açúcar", "chocolate", "ovos"],
+        "modo_preparo": "Misture tudo e asse por 40 minutos."
+    }
+]
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
